5274d32ecbfea62cfdc72b160ae5143ca6c86167
martilopez <marti.lopez.p@gmail.com>
2020-04-02 18:22:27 +0200

    librerias funciona

--- Letris original/2. Platforms/0. Windows Desktop/boto.cpp
+++ Letris original/2. Platforms/0. Windows Desktop/boto.cpp
@@ -1,5 +1,26 @@
 #include "boto.h"
 
+
+Sprite boto("./data/GUI/Btn_Circle_Close_1_Normal.png");
+
 void boto::settype(bool type) {
 	m_type = type;
+}
+
+void boto::setPosInicial(int x, int y) {
+	m_posButton_X = x;
+	m_posButton_Y = y;
+}
+
+void boto::drawBoto() {
+    boto.draw(m_posButton_X, m_posButton_Y);
+}
+
+{
+    if (mousePosX > posButton_X && mousePosX < posButton_X + 90 &&
+        mousePosY > posButton_Y && mousePosY < posButton_Y + 90 &&
+        moousePressed)
+    {
+        m_active = !m_active;
+    }
 }
--- Letris original/2. Platforms/0. Windows Desktop/boto.h
+++ Letris original/2. Platforms/0. Windows Desktop/boto.h
@@ -1,11 +1,15 @@
+#include "0. C++ Code/Graphic Lib/libreria.h"
+
 
 class boto
 {
 public:
 	void settype(bool type);
-
+	
 private:
 	bool m_active;
 	bool m_type;
+	int m_posButton_X;
+	int m_posButton_Y;
 };
 
